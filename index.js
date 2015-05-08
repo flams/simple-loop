c22f4f67dac3fc5de9d438920082e929e9fbd07f
Olivier Scherrer <pode.fr@gmail.com>
2015-05-07 23:51:37 -0400

    Re-introduce assert function

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 /**
-* @license simple-loop https://github.com/flams/simple-loop
-*
-* The MIT License (MIT)
-*
-* Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
-*/
+ * @license simple-loop https://github.com/flams/simple-loop
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+ */
 "use strict";
+
+function assert(assertion, error) {
+    if (assertion) {
+        throw new TypeError("simple-loop: " + error);
+    }
+}
 
 /**
  * Small abstraction for looping over objects and arrays
@@ -16,22 +22,20 @@
  * @param {Object} scope the scope in which to execute the callback
  */
 module.exports = function loop(iterated, callback, scope) {
-  if (typeof iterated != "object") throw new TypeError("simple-loop: " +
-    "iterated must be an array/object"
-  );
-  if (typeof callback != "function") throw new TypeError("simple-loop: " +
-    "callback must be a function"
-  );
+    assert(typeof iterated != "object", "iterated must be an array/object");
+    assert(typeof callback != "function", "callback must be a function");
 
-  if (Array.isArray(iterated)) {
-      for (var i=0; i<iterated.length; i++) {
-          callback.call(scope, iterated[i], i, iterated);
-      }
-  } else {
-      for (var i in iterated) {
-          if (iterated.hasOwnProperty(i)) {
-              callback.call(scope, iterated[i], i, iterated);
-          }
-      }
-  }
+    var i;
+
+    if (Array.isArray(iterated)) {
+        for (i = 0; i < iterated.length; i++) {
+            callback.call(scope, iterated[i], i, iterated);
+        }
+    } else {
+        for (i in iterated) {
+            if (iterated.hasOwnProperty(i)) {
+                callback.call(scope, iterated[i], i, iterated);
+            }
+        }
+    }
 };
